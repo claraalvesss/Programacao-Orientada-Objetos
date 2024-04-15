874d9179b1f6e35acc2533f106eac6d7a3eeb05c
Maria Clara Alves <166509484+claraalvesss@users.noreply.github.com>
2024-04-15 11:43:58 -0300

    Delete Estrutura_de_comparação directory

--- Estrutura_de_comparação/2º Questão.py
+++
@@ -1,7 +0,0 @@
-sexo = str(input("Insira um sexo: "))
-if sexo == 'M':
-  print("O sexo que você digitou é Masculino!")
-elif sexo == 'F':
-  print("O sexo que você digitou é Feminino!")
-else:
-  print("Sexo Inválido!")
