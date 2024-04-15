8b570e3208af2c1019e25fb4bfcf1afd01cbb010
Maria Clara Alves <166509484+claraalvesss@users.noreply.github.com>
2024-04-15 11:44:13 -0300

    Delete Listas directory

--- Listas/2º Questão.py
+++
@@ -1,5 +0,0 @@
-vetor = [int(input("Insira o {}º número: ".format(i + 1))) for i in range (10)]
-
-somadosquadrados = (sum(x**2 for x in vetor))
-
-print ("A soma dos quadrados é:", somadosquadrados)
