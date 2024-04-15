873de6565e61a9058b2fd54eaf044e6e97fe23b9
Maria Clara Alves <166509484+claraalvesss@users.noreply.github.com>
2024-04-15 11:44:06 -0300

    Delete Funções directory

--- Funções/4º Questão.py
+++
@@ -1,7 +0,0 @@
-def inverte_string(frase):
-  frase = frase[::-1]
-  return frase
-
-oracao = str(input("Insira uma frase aí: "))
-resultado = inverte_string(oracao)
-print("A frase '{}' invertida é: '{}'".format(oracao, resultado))
